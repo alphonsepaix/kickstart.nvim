81ab169719281bbc5258b8938c2cfd245f00af20
Alphonse Paix <alphonse.paix@outlook.com>
2024-11-15 18:34:17 +0100

    Formatters and LSPs

--- init.lua
+++ init.lua
@@ -712,11 +712,17 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        python = { 'autopep8' },
+        python = { 'isort', 'autopep8' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
         javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescript = { 'prettier' },
+        html = { 'prettier' },
+        css = { 'prettier' },
+        yaml = { 'prettier' },
+        json = { 'prettier' },
       },
     },
   },
